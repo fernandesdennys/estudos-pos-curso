e3b27fee2b294141a2732f935ce78a06607a6a2c
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-03-15 14:37:08 -0300

    script voltar ao topo

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,6 @@
-// Aguarda o carregamento completo do DOM antes de executar o código
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("O script foi carregado!"); // Exibe uma mensagem no console para indicar que o script foi carregado corretamente
-
-  // Seleciona o botão com o ID "btn" e adiciona um evento de clique
-  document.getElementById("btn").addEventListener("click", function () {
-    console.log("Botão clicado!"); // Exibe uma mensagem no console quando o botão é clicado
-
-    // Faz a página rolar suavemente para o topo
-    window.scrollTo({
-      top: 0, // Define o destino da rolagem no topo da página (posição 0)
-      behavior: "smooth", // Define que a rolagem deve ser suave, ao invés de instantânea
-    });
+function VoltarAoTopo() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Faz a rolagem suave
   });
-});
+}
